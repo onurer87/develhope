eed717d59d64b42cfc2b3f2a27dcc4183533686f
onurer87 <onur.ertugrul@outlook.com>
2023-01-24 16:01:57 +0300

    creating files

--- Exercises/mkdir.py
+++ Exercises/mkdir.py
@@ -1,5 +1,5 @@
-for i in range(13):
-    filename='Python_Part1_Variables_ex.'+str(i)+'.py'
+for i in range(1,5):
+    filename='./develhope/Exercises/Python_Part1_Conditions_ex.'+str(i)+'.py'
     print(filename)
     open(filename,'x')
 
